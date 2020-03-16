e7579eec0edf2e494160a7c76e927f87c336ac8a
Marcela Mallett <marcelamallett@Marcelas-MacBook-Air.local>
2020-03-16 16:44:19 -0500

    fixing namespaces

---
+++ BlackJack/Player/Hand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using BlackJack.Card;
+
+namespace BlackJack.Player
+{
+    class Hand
+    {
+        private List<Card.Card> cards;
+
+        public Hand(){ this.cards = new List<Card.Card>(); }
+
+        public List<Card.Card> GetCards() { return this.cards; }
+
+        public void SetCards(List<Card.Card> cards) { this.cards.AddRange(cards); }
+
+        public void AddCard(Card.Card card) { this.cards.Add(card); }
+    }
+}
